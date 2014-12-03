f899d5928c707d593e11b06242e43e2ade8e3f30
levelfour <levelfourslv@gmail.com>
2014-12-04 04:02:48 +0900

    add comments to linear_svm

--- svm/linear_svm.py
+++ svm/linear_svm.py
@@ -2,23 +2,24 @@
 # coding: utf-8
 
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn import datasets
 from sklearn import metrics
 from sklearn import cross_validation
 
-cmax = 1000
+cmax = 1000	# 最急降下法の試行回数
 
 class LinearSVM:
 	def __init__(self, dim, eta):
-		self.dim = dim
-		self.eta = eta
-		self.a = None
-		self.w = np.zeros((dim,))
+		self.dim = dim	# 学習データの次元数
+		self.eta = eta	# 最急降下法の学習率
+		self.a = None	# 未定乗数ベクトル
+		self.w = np.zeros((dim,))	# 識別関数
 
 	def fit(self, X, Y):
-		data_n = X.shape[0]
+		data_n = X.shape[0]	# 学習データ数
+		# 未定乗数ベクトルαの初期値は0ベクトルとする
 		self.a = np.zeros((data_n,))
+		# ones = (1,1,...,1)というベクトルを便宜上用意する
 		ones = np.array([1 for _ in range(data_n)])
 		H = np.array([[Y[i]*Y[j]*np.dot(X[i],X[j]) \
 				for i in range(data_n)] for j in range(data_n)])
@@ -26,8 +27,6 @@ class LinearSVM:
 		def L(av): return np.dot(self.a,ones)-self.a.T.dot(H.dot(self.a))/2.
 		# Lの導関数
 		def dL(av): return ones - np.dot(H,av)
-		# KKT条件を満たしているか
-		def kkt(): return np.dot(self.a,Y)>=0
 
 		# 勾配法でLを最大化→αを求める
 		for _ in range(cmax):
